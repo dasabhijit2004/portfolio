7a1b5abaa4ee20cd062818830a18c801225f6d22
Abhijit Das <sadabhijit2004@gmail.com>
2025-06-08 17:51:01 +0530

    Modified projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -73,9 +73,9 @@ const projectsData = [
     }
   },
   {
-    title: "Posture",
+    title: "Poseture",
     description: "A deep learning model based website that captures your postures.",
-    image: "/postrue.png",
+    image: "/postrue.avif",
     githubLink: "https://github.com/dasabhijit2004/posetrue",
     liveDemo: "https://posetrue.netlify.app",
     category: "ai",
